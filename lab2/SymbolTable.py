7ee870ee7fe79944fed1f5a85a637aaf4ce9b82f
Popa Andrei <kalinpopa@outlook.com>
2022-10-30 23:20:25 +0200

    modify getPos() function

--- lab2/SymbolTable.py
+++ lab2/SymbolTable.py
@@ -4,7 +4,7 @@ class SymbolTable:
         self.__items = [[]] * capacity
         self.__capacity = capacity
 
-    def getHash(self, key):
+    def __getHash(self, key):
         hash = 0
         for char in key:
             hash += ord(char) - ord('0')
@@ -13,29 +13,29 @@ class SymbolTable:
         return hash
 
     def add(self, key):
-        if self.contains(key):
+        if self.__contains(key):
             return self.getPos(key)
-        self.__items[self.getHash(key)].append(key)
+        self.__items[self.__getHash(key)].append(key)
 
         return self.getPos(key)
 
-    def contains(self, key):
-        return key in self.__items[self.getHash(key)]
+    def __contains(self, key):
+        return key in self.__items[self.__getHash(key)]
 
     def remove(self, key):
-        self.__items[self.getHash(key)].remove(key)
+        self.__items[self.__getHash(key)].remove(key)
 
     def getPos(self, key):
-        hashPos, listPos = self.getHash(key), 0
+        hashPos, listPos = self.__getHash(key), 0
         for item in self.__items[hashPos]:
             if item != key:
                 listPos += 1
             else:
                 break
 
-        return hashPos, listPos
+        return hashPos * self.__capacity + listPos
 
-operators = SymbolTable(100)
+operators = SymbolTable(10)
 
 print(operators.add("-"))
 print(operators.add("+"))
@@ -47,7 +47,7 @@ print(operators.getPos("=="))
 
 
 print()
-separators = SymbolTable(100)
+separators = SymbolTable(10)
 
 print(separators.add(";"))
 print(separators.add("\n"))
