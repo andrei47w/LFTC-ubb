87daabb8918f575c1f4fa3ecf772899be74e5232
Andrei Popa <andrei.popa@analog.com>
2022-12-05 14:45:02 +0200

    lab 5: initial commit

---
+++ lab5/main.py
@@ -0,0 +1,6 @@
+class Grammar:
+    def __init__(self, N, E, P, S):
+        self.N = N  # Non-terminals
+        self.E = E  # Terminals
+        self.P = P  # Productions
+        self.S = S  # Starting point
