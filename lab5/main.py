4ad99182e1b37e134a5ec78bd18f84fdf1b4785f
Andrei Popa <andrei.popa@analog.com>
2022-12-05 15:18:33 +0200

    lab 5: grammar without check

--- lab5/main.py
+++ lab5/main.py
@@ -1,6 +1,62 @@
+from FA import *
+
+
 class Grammar:
-    def __init__(self, N, E, P, S):
+    def __init__(self, N: list = [], E: list = [], P: list = [], S: str = "") -> None:
         self.N = N  # Non-terminals
         self.E = E  # Terminals
         self.P = P  # Productions
         self.S = S  # Starting point
+
+    def __str__(self):
+        return "N = { " + ', '.join(self.N) + " }\n" \
+            "E = { " + ', '.join(self.E) + " }\n" \
+            "P = { " + str(self.P) + " }\n" \
+            "S = { " + self.S + " }"
+
+    def setN(self, N):
+        self.N = N
+
+    def setE(self, E):
+        self.E = E
+
+    def setP(self, P):
+        self.P = P
+
+    def setS(self, S):
+        self.S = S
+
+    @staticmethod
+    def parseLine(line):
+        return line.strip().split(' ')[2:]
+
+    def readFile(self, file_name):
+        with open(file_name) as file:
+            N = Grammar.parseLine(file.readline())
+            E = Grammar.parseLine(file.readline())
+            S = Grammar.parseLine(file.readline())[0]
+
+            file.readline()
+            P = []
+            for line in file:
+                lhs, rhs = line.split('->')
+                lhs = lhs.strip()
+                rhs = [value.strip() for value in rhs.split('|')]
+
+                for value in rhs:
+                    P.append((lhs, value))
+
+            self.setN(N)
+            self.setP(P)
+            self.setE(E)
+            self.setS(S)
+
+    def checkCFG(self):
+        return True
+
+
+grammar = Grammar()
+grammar.readFile("g2.txt")
+
+print(str(grammar))
+print(grammar.checkCFG())
