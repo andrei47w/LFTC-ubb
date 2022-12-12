2203b9730bd4bf83d5a7bdd2fb9b3d0e6259dcc0
916-Pop-Felix <felcris2001@gmail.com>
2022-12-12 15:16:00 +0200

    lab 6 almost done

--- lab6/Parser.py
+++ lab6/Parser.py
@@ -9,3 +9,32 @@ class ParserConfig:
         self.i = 0
         self.alpha = []
         self.beta = []
+
+    def expand(self):
+        current = self.beta.pop()
+        production = self.grammar.getProductionsForNonTerminal(current)
+        rules = production[0][1].split()[0]
+        if rules != "epsilon":
+            self.beta += reversed(rules)
+        self.beta.append(rules + "#0")
+
+    def advance(self):
+        self.i += 1
+        terminal = self.beta.pop()
+        self.alpha.append(terminal)
+
+
+class ParserTests:
+    def testAdvance(self, parser):
+        parser.beta.append('a')
+        parser.advance()
+        assert (len(parser.beta) == 0)
+        assert (parser.alpha.pop() == 'a')
+        assert (parser.i == 1)
+        parser.i -= 1
+
+    def testExpand(self,parser):
+        parser.beta.append('S')
+        parser.expand()
+        assert (parser.beta.pop() == 'aA#0')
+
